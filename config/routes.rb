7dfcd039cb31b2e196e8fc2cad895e077fd53c56
Little Boll Weevil <littlebollweevil@swbell.net>
2011-01-18 09:18:57 -0600

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 SampleApp::Application.routes.draw do
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
+  
+  root :to => 'pages#home'
+  
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
+  get "users/new"
+  
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
